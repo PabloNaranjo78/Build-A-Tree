2e3ef8aa40b4559abd8423dd5a9c94b59e6e0ac0
PabloNaranjo78 <67924944+PabloNaranjo78@users.noreply.github.com>
2020-12-10 20:00:56 -0600

    Prueba commit

--- Build-A-Tree-GUI/Assets/Scripts/Tokens/BTree_Cu.cs
+++ Build-A-Tree-GUI/Assets/Scripts/Tokens/BTree_Cu.cs
@@ -11,6 +11,7 @@ public class BTree_Cu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
         gameLevelManager = FindObjectOfType<LevelManager> ();
     }
 
