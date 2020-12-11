217e58f975f32417600a51c3c6b6f723dd8389e7
dlurena24 <dlurena24@gmail.com>
2020-12-10 20:33:51 -0600

    Revert "Prueba commit"
    This reverts commit 2e3ef8aa40b4559abd8423dd5a9c94b59e6e0ac0.

--- Build-A-Tree-GUI/Assets/Scripts/Tokens/BTree_Cu.cs
+++ Build-A-Tree-GUI/Assets/Scripts/Tokens/BTree_Cu.cs
@@ -11,7 +11,6 @@ public class BTree_Cu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
         gameLevelManager = FindObjectOfType<LevelManager> ();
     }
 
